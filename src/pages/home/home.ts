233239b73116d039f42530dd6fc9b99d38099d16
Jon Pecson <jonpecson.io@gmail.com>
2018-06-10 00:37:49 +0800

    UPDATE - PIN CODE Theme

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController
+    , public modelCtrl: ModalController) {
 
   }
 
+  Pin: String ="";
+  ShowPin: Boolean = false;
+  
+  eventCapture(event) {
+    this.ShowPin = false;
+    this.Pin=event;
+  }
+
+  showPin() {
+    this.ShowPin = !this.ShowPin;
+  }
 }
